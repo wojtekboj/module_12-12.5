1c377b658688f3b9d553b9146d0864e0dfad9ea7
Wojtek Bojarczuk <wojtekboj@gmail.com>
2018-06-21 09:05:58 +0200

    Modification scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 'use strict';
 
 // Variable "tweetLink" for sending tweets and a variable "quoteUrl" for retrieving quotes
-var tweetLink = "https://twitter.com/intent/tweet?text=";
-var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var tweetLink = 'https://twitter.com/intent/tweet?text=';
+var quoteUrl = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
 
 // Function that retrieves the quote
 function getQuote() {
     fetch(quoteUrl, {
-            cache: "no-store"
+            cache: 'no-store'
         })
         .then(function (resp) {
             return resp.json();
@@ -25,17 +25,17 @@ function createTweet(input) {
     var quoteAuthor = data.title;
 
     if (!quoteAuthor.length) {
-        quoteAuthor = "Unknown author";
+        quoteAuthor = 'Unknown author';
     }
 
-    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+    var tweetText = 'Quote of the day - ' + quoteText + ' Author: ' + quoteAuthor;
 
     if (tweetText.length > 140) {
         getQuote();
     } else {
         var tweet = tweetLink + encodeURIComponent(tweetText);
         document.querySelector('.quote').innerText = quoteText;
-        document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+        document.querySelector('.author').innerText = 'Author: ' + quoteAuthor;
         document.querySelector('.tweet').setAttribute('href', tweet);
     }
     document.querySelector('.tweet').setAttribute('href', tweet);
